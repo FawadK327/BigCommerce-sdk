41741a666d5858fca686e311be95221eba350013
Fawad Khan <fawad.khan@qisstpay.com>
2022-10-20 11:37:41 +0500

    Changes to BigCommerce sdk app

--- src/checkout-loader6.js
+++ src/checkout-loader6.js
@@ -1,4 +1,5 @@
 /******************* Including External CSS file Starts ******************/
+// import 'bootstrap/dist/css/bootstrap.min.css';
 var cssId = 'myCss';  // you could encode the css path itself to generate id..
 if (!document.getElementById(cssId))
 {
@@ -10,6 +11,13 @@ if (!document.getElementById(cssId))
   link.href = 'https://qisstpaysnadbox-bigcommerce.s3.ap-southeast-1.amazonaws.com/bigCommerceOneClick.css';   //Path where external css file is deployed
   link.media = 'all';
   head.appendChild(link);
+  /******************* Including Bootstrap link Starts ******************/
+  var bootstrapLink = document.createElement('link');
+  bootstrapLink.rel="stylesheet";
+  bootstrapLink.href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css";
+  bootstrapLink.crossorigin="anonymous";
+  head.appendChild(bootstrapLink);
+/******************* Including Bootstrap link Ends ******************/
 }
 /******************* Including External CSS file End ******************/
 
@@ -39,8 +47,8 @@ var bigCommerce_checkout = null;
         
         if(cartObj){
           
-          // let qisstpay_merchant_token = btoa(location.hostname);  //Merchant token generated on backend
-          let qisstpay_merchant_token = 'cWlzc3RwYXkubXliaWdjb21tZXJjZS5jb20=';
+          let qisstpay_merchant_token = btoa(location.hostname);  //Merchant token generated on backend
+          // let qisstpay_merchant_token = 'cWlzc3RwYXkubXliaWdjb21tZXJjZS5jb20=';
           //   console.log('hostname :',location.hostname,'url: ', 'https://'+location.hostname);
           var qisstpay_products = [];
           var totalPrice = cartObj.baseAmount;   //Cart total price after discount and shippingCost
@@ -99,19 +107,20 @@ var bigCommerce_checkout = null;
           //*********************************************************************//
           
           url = `https://dev-apis.qisstpay.com/?identity-token=`+qisstpay_merchant_token+'&queryUrl='+queryUrl; //Url to be passed to the iframe src attribute
-    
           qisstpay_modal = `
-            <div class="qp8911_modal custom_modal_by_me" id="qp8911_bootstrapModal_checkout" role="dialog">
-              <div class="qp8911_modal-dialog qp8911_modal-dialog-centered" role="document" >
-                <div class="qp8911_modal-content col-md-6 col-lg-4">
-                  <!-- Modal Header -->
-                  <!-- Modal Body -->
-                  <div class="qp8911_modal-body teez" style="border-radius: 140px;">
-                    <div class="qp-lds-roller" id="qp-lds-roller">
-                      <lottie-player src="'.plugins_url( 'js/animation_qp_logo.json', __FILE__ ).'" background="transparent"  speed="1"  style="width: 300px; height: 300px;" loop autoplay></lottie-player>
-                    </div>
-                    <iframe id="qisttpayifram" class="qisttpayifram" width="100%" height="600"  src='`+url+`'  frameborder="0" allowpaymentrequest allowfullscreen style="background: #FFFFFF;border-radius: 22px;padding: 0px;" ></iframe>
-                  </div>                      
+            <div id="qp-checkout-modal">
+              <div class="qp8911_modal modal custom_modal_by_me" id="qp8911_bootstrapModal_checkout" role="dialog">
+                <div class="qp8911_modal-dialog modal-dialog qp8911_modal-dialog-centered" role="document" >
+                  <div class="qp8911_modal-content modal-content col-md-6 col-lg-4">
+                    <!-- Modal Header -->
+                    <!-- Modal Body -->
+                    <div class="qp8911_modal-body modal-body teez" style="border-radius: 140px;">
+                      <div class="qp-lds-roller" id="qp-lds-roller">
+                        <lottie-player src="'.plugins_url( 'js/animation_qp_logo.json', __FILE__ ).'" background="transparent"  speed="1"  style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+                      </div>
+                      <iframe id="qisttpayifram" class="qisttpayifram" width="100%" height="600"  src='`+url+`'  frameborder="0" allowpaymentrequest allowfullscreen style="background: #FFFFFF;border-radius: 22px;padding: 0px;" ></iframe>
+                    </div>                      
+                  </div>
                 </div>
               </div>
             </div>`;
@@ -133,11 +142,11 @@ var bigCommerce_checkout = null;
   el.src = 'https://checkout-sdk.bigcommerce.com/v1/loader.js';   //Loading checkout sdk in script tag
   document.head.appendChild(el);
 
-  /***************** Creating Iframe modal starts ******************/
+  /***************** Creating HTML starts ******************/
   function htmlToElement(html) {
       var template = document.createElement('template');
       html = html.trim(); // Never return a text node of whitespace as the result
       template.innerHTML = html;
       return template.content.firstChild;
   }
-  /***************** Creating Iframe modal Ends ******************/
+  /***************** Creating HTML Ends ******************/
